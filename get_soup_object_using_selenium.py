25e93b25cc176410419ea9da8e681ab7e9582992
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-07-27 21:04:31 +0500

    new

---
+++ get_soup_object_using_selenium.py
@@ -0,0 +1,50 @@
+#!/usr/bin/ipython3
+def get_soup_object_using_selenium(url, visual=False, urls=True):
+	from selenium import webdriver
+	from bs4 import BeautifulSoup
+	
+	# url = input("Enter your url: ")
+	# visusl = True if input("Visual or not? [y|n]") in ['y', 'Y'] else False
+
+	if visual:
+		browser = webdriver.Firefox(executable_path = "geckodriver")
+	else:
+		from selenium.webdriver.firefox.options import Options
+		options = Options()
+		options.add_argument("--headless")
+		browser = webdriver.Firefox(executable_path = "geckodriver", options=options)
+
+	browser.get(url)
+	s = BeautifulSoup(browser.page_source, "lxml")
+	extrected_urls = []
+	if urls:
+		for i in s.select("a"):
+			try:
+				l = i['href']
+				extrected_urls.append(l)
+			except:
+				pass
+		for i in s.select("source"):
+			try:
+				l = i['src']
+				extrected_urls.append(l)
+			except:
+				pass
+		print(f"\nA tuple is returned, 1st vlues is urls, 2nd value is BeautifulSoup object\nThere are {len(extrected_urls)} urls\n")
+	return (set(extrected_urls), s)
+
+
+
+def get_all_links(url, pattern=""):
+	from bs4 import BeautifulSoup
+	import requests
+	import re
+	
+	s = BeautifulSoup(requests.get(url).text, 'lxml')
+	x2= s.find_all('a', href=re.compile(pattern))
+	x2 = [i['href'] for i in x2]
+	se = []
+	for i in x2:
+		if not i in se:
+			se.append(i)
+	return se
