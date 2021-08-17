4200ba529b0abac32c21b53a7087aa9d663f8144
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-18 01:20:52 +0500

    u

--- open_html.py
+++ open_html.py
@@ -8,16 +8,16 @@ import json
 from datetime import datetime
 
 def func_(vid, img, channel, upload_date):
-	
 	upload_date = datetime.strptime(upload_date, "%Y%m%d")
 	uploaded_before_days = (datetime.now() - upload_date).days
-	if uploaded_before_days < 31 or (uploaded_before_days > 365):
-		msg = f"{uploaded_before_days} days ago"
-	else:
-		msg = f"{uploaded_before_days//31} months ago"
+	# if uploaded_before_days < 31 or (uploaded_before_days > 365):
+	msg = f"{uploaded_before_days} days ago"
+	# else:
+		# msg = f"{uploaded_before_days//31} months ago"
 
 	vid_name = vid.strip(".webm").strip(".mkv").strip(".mp4").replace("_", ' ').capitalize()
 	if vid.endswith(".part"):
+		# print("1")
 		return ""
 	# img = f'/home/{getpass.getuser()}/thumbnail/{img.replace("/", "_")}'
 	vid = f'/home/home/Videos/{vid}'
@@ -28,10 +28,13 @@ def func_(vid, img, channel, upload_date):
 			vid = vid_
 			break
 	else:
+		# print("2")
 		return ""
 	if vid in videos_to_exclude:
+		# print("3")
 		return ""
 	if int(list(os.popen(f"du -s -BM {vid} | cut -dM -f1"))[0].strip()) < 10:
+		# print("4")
 		return ""
 	# if vid.endswith(".mkv"):
 		# return ""
@@ -86,7 +89,7 @@ try:
 	x = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
 	# x = sorted(x.items(), key=lambda x: x[1]['upload_date'], reverse=True) # sort by upload date 
 	x = sorted(x.items(), key=lambda x: (datetime.now() - datetime.strptime(x[1]['upload_date'], "%Y%m%d")).days)
-	x = {i[0] : i[1] for i in x[:150]} # only last 150 videos
+	x = {i[0] : i[1] for i in x}
 	to_be_exclude = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_be_exclude.json", "r"))
 	channels_to_exclude = to_be_exclude['channel']
 	videos_to_exclude = to_be_exclude['video']
