bcaefa961e3864661ce842f40c1816fa173f9289
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-28 18:35:50 +0500

    u

--- Kids_Vids_Jango/auth_app/views.py
+++ Kids_Vids_Jango/auth_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+import getpass
+import os
+import shutil
+import pickle
+import sys
+import json
 
-# Create your views here.
+def auth_(request):
+	print('................ auth_app.auth_ called')
+	return render(request, 'auth.html')
+
+
+def auth_check(request):
+	print("................ auth_app.auth_check called")
+	password = request.POST['user_password']
+	
+
+	# should be corrected  # amir
+	# if password.lower().strip() == 'ayesha123':
+	if True:
+
+		# os.system(f"/home/{getpass.getuser()}/github/Kids_Vids/open_html.py")
+		# shutil.copy(f"/home/{getpass.getuser()}/github/Kids_Vids/dashboard.html", f"/home/{getpass.getuser()}/github/Kids_Vids/Kids_Vids_Jango/auth_app/templates/dashboard.html")
+		# print("................ rendering dashboard.html page") 
+		# return render(request, 'dashboard.html')
+
+		sys.path += [f'/home/{getpass.getuser()}/github/Kids_Vids']
+		from get_current_channels import get_channels
+
+		channels = get_channels()
+		channels_mapping = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/channels_mapping.txt", "r"))
+		list_of_channels = [channels_mapping[i[0]] for i in channels]
+
+		return render(request, 'select_channels/choose_channels.html', {"channels" : list_of_channels})
+ 
+	else:
+		return render(request, 'Error.html', {"error" : "Wrong password"})
