4d9f8f91bdd79c9c3e1421414c53aace7717eb25
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-29 00:42:45 +0500

    u

--- Kids_Vids_Jango/select_channels/views.py
+++ Kids_Vids_Jango/select_channels/views.py
@@ -9,7 +9,11 @@ import pandas as pd
 import os
 from datetime import datetime
 
-def func_(vid, img, channel, upload_date, videos_to_exclude):
+to_be_exclude = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_be_exclude.json", "r"))
+channels_to_exclude = to_be_exclude['channel']
+videos_to_exclude = to_be_exclude['video']
+
+def func_(vid, img, channel, upload_date):
 	upload_date = datetime.strptime(upload_date, "%Y%m%d")
 	uploaded_before_days = (datetime.now() - upload_date).days
 	msg = f"{uploaded_before_days} days ago"
@@ -27,8 +31,8 @@ def func_(vid, img, channel, upload_date, videos_to_exclude):
 		return ""
 	if vid in videos_to_exclude:
 		return ""
-	if int(list(os.popen(f"du -s -BM {vid} | cut -dM -f1"))[0].strip()) < 10:
-		return ""
+	# if int(list(os.popen(f"du -s -BM {vid} | cut -dM -f1"))[0].strip()) < 10:
+		# return ""
 	if vid.endswith(".mkv"):
 		  return f"""<div class="column">
 				<figure class="D3Oi9">
@@ -58,7 +62,7 @@ def func_(vid, img, channel, upload_date, videos_to_exclude):
 				</figure>
 			</div>
 			"""
-				
+
 def select_channels(request):
 	print("................. select_channels.select_channels called")
 
@@ -67,37 +71,21 @@ def select_channels(request):
 	selected_channels = [k.strip() for k,v in x.items() if v == ['on']]
 	if not selected_channels:
 		return render(request, 'Error.html', {"error" : "No channel is selected!!!!!!"})
-	channels_mapping = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/channels_mapping.txt", "r"))
-	channels_mapping_flapped = {v:k for k,v in channels_mapping.items()}
-	selected_channels = [channels_mapping_flapped[i] for i in selected_channels if i in channels_mapping_flapped.keys()]
-
-
-	sys.path += [f'/home/{getpass.getuser()}/github/Kids_Vids']
-	from get_current_channels import get_channels
-	channels = get_channels()
-	channels = [i for i in channels if i[0] in selected_channels]
-	if not channels:
-		return render(request, 'Error.html', {"error" : "No channel is selected_!!!!"})
-
-
-	##############################################################################################################
 	try:
 		x = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
 		x = sorted(x.items(), key=lambda x: (datetime.now() - datetime.strptime(x[1]['upload_date'], "%Y%m%d")).days)
 		x = {i[0] : i[1] for i in x}
-		to_be_exclude = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_be_exclude.json", "r"))
-		channels_to_exclude = to_be_exclude['channel']
-		videos_to_exclude = to_be_exclude['video']
 
-		channels_mapping = json.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/channels_mapping.txt", "r"))
+		df = pd.DataFrame.from_dict(x, orient='index')
+		df = df[(df.channel.isin(selected_channels)) & df.downloaded]
+		df.upload_date = pd.to_datetime(df.upload_date)
+		if len(df) < 200:
+			x = df.reset_index().rename(columns={"index" : "url"}).groupby("channel").apply(lambda x:x.sort_values("upload_date", ascending=False)).reset_index(drop=True).set_index('url').sort_values("upload_date", ascending=False)
+		else:
+			x = df.reset_index().rename(columns={"index" : "url"}).groupby("channel").apply(lambda x:x.sort_values("upload_date", ascending=False).iloc[:15]).reset_index(drop=True).set_index('url').sort_values("upload_date", ascending=False)
+		x.upload_date = x.upload_date.astype(str).str.replace("-", "")
+		x = x.to_dict(orient="index")
 
-		to_remove = []
-		for k,v in x.items():
-			if v['channel'] in channels_to_exclude or (k in videos_to_exclude):
-				to_remove.append(k)
-		if to_remove:
-			for i in to_remove:
-				x.pop(i)
 
 		s = """<!DOCTYPE html><html>
 		<head>
@@ -112,34 +100,20 @@ def select_channels(request):
 			<center>Kids_Vids</center><br>
 			<div class="row">
 				"""
-
-		df = pd.DataFrame.from_dict(x, orient='index')
-		df = df[df.downloaded]
-		df.upload_date = pd.to_datetime(df.upload_date)
-		x = df.reset_index().rename(columns={"index" : "url"}).groupby("channel").apply(lambda x:x.sort_values("upload_date", ascending=False).iloc[:15]).reset_index(drop=True).set_index('url').sort_values("upload_date", ascending=False)
-		# x = df.reset_index().rename(columns={"index" : "url"}).groupby("channel").apply(lambda x:x.sort_values("upload_date", ascending=False)).reset_index(drop=True).set_index('url').sort_values("upload_date", ascending=False)
-		x.upload_date = x.upload_date.astype(str).str.replace("-", "")
-		x = x.to_dict(orient="index")
-
 		for k,v in x.items():
 			s += func_(
 				vid = v['video_name'], 
 				# img = v['thumbnail_url'], # fatching from internet
 				img = f"/home/{getpass.getuser()}/github/Kids_Vids/thumbs/{v['thumbnail_name']}", 
 				channel = v['channel'],
-				upload_date = v['upload_date'],
-				videos_to_exclude=videos_to_exclude
+				upload_date = v['upload_date']
 				)
 		s += "\n</div></body></html>"
 
-		open("dashboard.html", 'w').write(s)
-		#import webbrowser
-		#webbrowser.get("chromium").open(f"/home/{getpass.getuser()}/github/Kids_Vids/dashboard.html")
-		# os.system(f"chromium /home/{getpass.getuser()}/github/Kids_Vids/dashboard.html")
+		open(f"/home/{getpass.getuser()}/github/Kids_Vids/Kids_Vids_Jango/open_seleted_channels/templates/dashboard.html", 'w').write(s)
+
 	except Exception as e:
 		open(f"/home/{getpass.getuser()}/github/Kids_Vids/EX.txt", 'w').write(str(e))
 		return render(request, 'Error.html', {"error" : e})
-
-	##############################################################################################################
 
 	return render(request, 'dashboard.html')
