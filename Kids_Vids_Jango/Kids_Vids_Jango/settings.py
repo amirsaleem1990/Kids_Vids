039ef861bed78015d9fe9331889d8378055a042e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-29 16:30:47 +0500

    moved the keyy to local file

--- Kids_Vids_Jango/Kids_Vids_Jango/settings.py
+++ Kids_Vids_Jango/Kids_Vids_Jango/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qu3*u&bu*b(l@0(9_mw4rkoxr45#_35-)*i9aes07mxz2lq7s)'
+SECRET_KEY = open("SECRET_KEY", 'r').read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
