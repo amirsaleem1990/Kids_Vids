16de28d40b7d6b1d07a7b88b5e0b815a091a7719
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-29 16:42:52 +0500

    secret key har dafa new ho gi, is ka ye nuqsan ho ga k cockies or history wager save nahi rahyn gy, magar mujhy is sy koi problem nahi h

--- Kids_Vids_Jango/Kids_Vids_Jango/settings.py
+++ Kids_Vids_Jango/Kids_Vids_Jango/settings.py
@@ -22,10 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open("SECRET_KEY", 'r').read().strip()
+# SECRET_KEY = open("SECRET_KEY", 'r').read().strip()
+# import secrets
+# SECRET_KEY = secrets.token_urlsafe(32)
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
