6ff812dd755e882ff7c99113ca2d36c6abe4027f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-17 14:41:11 +0500

    Added replace_wrong_videos_names_with_correct_one function, kuch videos esi hen jin ka extention mapping.pkl me galat hota h, to ye function un ko sahi kar day ga, actual mapping.pkl me kuch override nahi ho ga balky temporary modified copy ban gay gi

--- Kids_Vids_Jango/auth_app/views.py
+++ Kids_Vids_Jango/auth_app/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 from django.http.response import HttpResponse
 import getpass
@@ -6,6 +7,31 @@ import shutil
 import pickle
 import sys
 import json
+
+
+def replace_wrong_videos_names_with_correct_one(x : dict) -> dict:
+	data_vids = pd.DataFrame.from_dict(x, orient="index").video_name.to_list()
+	disk_vids = os.listdir("/home/home/Videos/")
+
+	files_in_disk_but_not_in_data = []
+	for i in disk_vids:
+		if not i in data_vids:
+			if not i.endswith(".part"):
+				files_in_disk_but_not_in_data.append(i)
+	if files_in_disk_but_not_in_data:
+		df = pd.DataFrame.from_dict(x, orient="index")
+		df.reset_index(drop=True, inplace=True)
+		df_copy = df.copy(deep=True)
+		r2 = df.video_name.reset_index(drop=True)
+		for disk_value in files_in_disk_but_not_in_data:
+			disk_value_without_extention = disk_value.split(".")[0]
+			q = r2.str.contains(disk_value_without_extention)
+			if q.sum() > 0:
+				data_value = r2.iloc[q.idxmax()]
+				if data_value.split(".")[0] == disk_value_without_extention:
+					df.loc[df.video_name.eq(data_value), "video_name"] = disk_value
+		x = df.to_dict(orient="index")
+	return x
 
 def auth_(request):
 	print('................ auth_app.auth_ called')
@@ -21,7 +47,7 @@ def auth_check(request):
 
 	# should be corrected  # amir
 	if password.lower().strip() == '03323388625':
-	# if True:
+		# if True:
 		# os.system(f"/home/{getpass.getuser()}/github/Kids_Vids/open_html.py")
 		# shutil.copy(f"/home/{getpass.getuser()}/github/Kids_Vids/dashboard.html", f"/home/{getpass.getuser()}/github/Kids_Vids/Kids_Vids_Jango/auth_app/templates/dashboard.html")
 		# print("................ rendering dashboard.html page") 
@@ -35,14 +61,13 @@ def auth_check(request):
 		list_of_channels = [channels_mapping[i[0]] for i in channels]
 
 		x = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
+		x = replace_wrong_videos_names_with_correct_one(x)
 		channels = {}
 		for url, data in x.items():
-			if not data['channel'] in list_of_channels:
-				continue
-			if data['downloaded']:
-				if os.path.exists(f"/home/{getpass.getuser()}/Videos/{data['video_name']}"):
+			if data['channel'] in list_of_channels and (os.path.exists(f"/home/home/Videos/{data['video_name']}")):
 					channels[data['channel']] = channels.get(data['channel'], 0) + 1
 		channels = [(k,v) for k,v in channels.items()]
+
 		# return render(request, 'select_channels/choose_channels.html', {"channels" : list_of_channels})
 		return render(request, 'select_channels/choose_channels.html', {"channels" : channels})
  
