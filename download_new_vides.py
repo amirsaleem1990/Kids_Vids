af2b5c779925310086ef4f1b262abb5f777bd40d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-12-01 17:42:55 +0500

    u

--- download_new_vides.py
+++ download_new_vides.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 import getpass
+from selenium.webdriver.firefox.options import Options
+from multiprocessing import Pool
+import itertools
+import re
+import time
+from datetime import timedelta
+import multiprocessing.dummy
+import subprocess
+import os
+import pickle
+import json
+from datetime import datetime
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import youtube_dl
+import traceback
+
+
 def extract_videos_from_channel_page(channel_url):
 	try:
 		browser.get(channel_url)
@@ -157,9 +175,7 @@ def get_urls_to_download(c):
 		errors[url] = ["download_thumbnails",e, str(datetime.now())]
 		mapping[url].append("Error")
 """
-
 def get_info(to_download):
-	print("\n---- get_info called ........")
 	for u in to_download:
 		# agar url ka data pehly fetch kya hwa h to dubara fetch nahi karo
 		if u in mapping:
@@ -224,138 +240,122 @@ def get_info(to_download):
 # 		errors[url] = ["download_videos fail",e, str(datetime.now())]
 
 
-
-from selenium.webdriver.firefox.options import Options
-from multiprocessing import Pool
-import itertools
-import re
-import time
-from datetime import timedelta
-import multiprocessing.dummy
-import subprocess
-import os
-import pickle
-import json
-from datetime import datetime
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import youtube_dl
-import traceback
-
-
-pkls = [i for i in os.listdir(f"/home/{getpass.getuser()}/github/Kids_Vids/") if i.startswith("to_download_") and i.endswith(".pkl")]
-if pkls:
-	for i in pkls:
-		os.remove(i)
-
-if not os.path.exists("/home/home/Videos/"):
-	raise Exception("No directory /home/home/Videos/")
-
-# if not os.path.exists("/home/home/thumbnail/"):
-if not os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/thumbs/"):
-	raise Exception(f"No directory /home/{getpass.getuser()}/github/Kids_Vids/thumbs/")
-
-if os.path.exists("to_be_exclude.json"):
-	excluded_videos = open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_be_exclude.json", 'r').read().splitlines()
-else:
-	excluded_videos = []
-
 if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl"):
 	mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
 else:
 	mapping = dict()
 
-if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/Error_file.pkl"):
-	errors = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error_file.pkl", 'rb'))
-else:
-	errors = dict()
+if __name__ == "__main__":
 
-# if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/downloaded.txt"):
-# 	downloaded = open(f"/home/{getpass.getuser()}/github/Kids_Vids/downloaded.txt", 'r').read().splitlines()
-# else:
-# 	downloaded = list()
-downloaded = [k for k,v in mapping.items() if v['downloaded']]
+	pkls = [i for i in os.listdir(f"/home/{getpass.getuser()}/github/Kids_Vids/") if i.startswith("to_download_") and i.endswith(".pkl")]
+	if pkls:
+		for i in pkls:
+			os.remove(i)
 
-get_urls_to_download_recursive_n = 0
+	if not os.path.exists("/home/home/Videos/"):
+		raise Exception("No directory /home/home/Videos/")
 
-to_skip = []
-# json_name_mapping = {}
+	# if not os.path.exists("/home/home/thumbnail/"):
+	if not os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/thumbs/"):
+		raise Exception(f"No directory /home/{getpass.getuser()}/github/Kids_Vids/thumbs/")
 
+	if os.path.exists("to_be_exclude.json"):
+		excluded_videos = open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_be_exclude.json", 'r').read().splitlines()
+	else:
+		excluded_videos = []
 
-from get_current_channels import get_channels
-channels = get_channels() 
+	if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/Error_file.pkl"):
+		errors = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error_file.pkl", 'rb'))
+	else:
+		errors = dict()
 
-# iter_ = list(itertools.chain.from_iterable(list(mapping.values())))
+	# if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/downloaded.txt"):
+	# 	downloaded = open(f"/home/{getpass.getuser()}/github/Kids_Vids/downloaded.txt", 'r').read().splitlines()
+	# else:
+	# 	downloaded = list()
+	downloaded = [k for k,v in mapping.items() if v['downloaded']]
 
+	get_urls_to_download_recursive_n = 0
 
-options = Options()
-options.add_argument("--headless")
-browser = webdriver.Firefox(executable_path = "geckodriver", options=options)
-
-pool = Pool()   # Create a multiprocessing Pool
-pool.map(get_urls_to_download, channels)
-
-browser.close()
-
-pkls = [i for i in os.listdir(f"/home/{getpass.getuser()}/github/Kids_Vids/") if i.startswith("to_download_") and i.endswith(".pkl")]
-to_download = []
-for i in pkls:
-	to_download += pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/{i}", 'rb'))
-to_download = [i for i in set(to_download) if not i in downloaded]
-pickle.dump(to_download, open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl", 'wb'))
-print(f"\n\n ---------------------------- to_download saved as /home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl\n\n")
-# to_download = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl", 'rb'))
-
-for i in pkls:
-	os.remove(f"/home/{getpass.getuser()}/github/Kids_Vids/{i}")
+	to_skip = []
+	# json_name_mapping = {}
 
 
-# # download_jsons(to_download)
-try:
-	get_info(to_download)
-except:
-	pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-	raise Exception(str(traceback.format_exc()))
-pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-print(f"\n\n ---------------------------- mapping saved as /home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl\n\n")
-# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
+	from get_current_channels import get_channels
+	channels = get_channels() 
 
-# extrect_data_from_json(to_download)
-# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
-
-# download_thumbnails(to_download) 
-# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
-
-if errors:
-	pickle.dump(errors, open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl", 'wb'))
-
-# to_download = [i for i in to_download if not i in to_skip]
-# urls in mapping but video is not_downloaded
-# excluded_videos = open("to_be_exclude.json", 'r').read().splitlines()
-# to_download = [k for k,v in mapping.items() if not os.path.exists(f"/home/home/Videos/{v['video_name']}") and (not i in to_skip) and (not i in excluded_videos)]
+	# iter_ = list(itertools.chain.from_iterable(list(mapping.values())))
 
 
-# moved to Download.py
-# try:
-# 	if to_download:
-# 		p = multiprocessing.dummy.Pool()
-# 		print( "\n---- download_videos called ........")
-# 		print(f"\n--------------------------------------------------- {len(to_download)} videos to be downloaded ..........\n\n")
-# 		p.map(download_videos, to_download)
-# except:
-# 	pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-# 	raise Exception(str(traceback.format_exc()))
-# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
-os.system(f"python3 /home/{getpass.getuser()}/github/Kids_Vids/Download.py")
+	options = Options()
+	options.add_argument("--headless")
+	browser = webdriver.Firefox(executable_path = "geckodriver", options=options)
+
+	pool = Pool()   # Create a multiprocessing Pool
+	pool.map(get_urls_to_download, channels)
+
+	browser.close()
+
+	pkls = [i for i in os.listdir(f"/home/{getpass.getuser()}/github/Kids_Vids/") if i.startswith("to_download_") and i.endswith(".pkl")]
+	to_download = []
+	for i in pkls:
+		to_download += pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/{i}", 'rb'))
+	to_download = [i for i in set(to_download) if not i in downloaded]
+	pickle.dump(to_download, open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl", 'wb'))
+	print(f"\n\n ---------------------------- to_download saved as /home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl\n\n")
+	# to_download = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/to_download.pkl", 'rb'))
+
+	for i in pkls:
+		os.remove(f"/home/{getpass.getuser()}/github/Kids_Vids/{i}")
 
 
-if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl"):
+	# # download_jsons(to_download)
 	try:
-		errors = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl", 'rb'))
-		if errors:
-			print(f"\n\n{'*'*15}ERRORS{'*'*15}")
-			print(errors, sep="\n")
+		print("Out function", id(mapping))
+		get_info(to_download)
 	except:
-		pass
+		pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+		raise Exception(str(traceback.format_exc()))
+	pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+	print(f"\n\n ---------------------------- mapping saved as /home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl\n\n")
+	# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
+
+	# extrect_data_from_json(to_download)
+	# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+	# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
+
+	# download_thumbnails(to_download) 
+	# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+	# mapping = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'rb'))
+
+	if errors:
+		pickle.dump(errors, open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl", 'wb'))
+
+	# to_download = [i for i in to_download if not i in to_skip]
+	# urls in mapping but video is not_downloaded
+	# excluded_videos = open("to_be_exclude.json", 'r').read().splitlines()
+	# to_download = [k for k,v in mapping.items() if not os.path.exists(f"/home/home/Videos/{v['video_name']}") and (not i in to_skip) and (not i in excluded_videos)]
+
+
+	# moved to Download.py
+	# try:
+	# 	if to_download:
+	# 		p = multiprocessing.dummy.Pool()
+	# 		print( "\n---- download_videos called ........")
+	# 		print(f"\n--------------------------------------------------- {len(to_download)} videos to be downloaded ..........\n\n")
+	# 		p.map(download_videos, to_download)
+	# except:
+	# 	pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+	# 	raise Exception(str(traceback.format_exc()))
+	# pickle.dump(mapping, open(f"/home/{getpass.getuser()}/github/Kids_Vids/mapping.pkl", 'wb'))
+	os.system(f"python3 /home/{getpass.getuser()}/github/Kids_Vids/Download.py")
+
+
+	if os.path.exists(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl"):
+		try:
+			errors = pickle.load(open(f"/home/{getpass.getuser()}/github/Kids_Vids/Error.pkl", 'rb'))
+			if errors:
+				print(f"\n\n{'*'*15}ERRORS{'*'*15}")
+				print(errors, sep="\n")
+		except:
+			pass
