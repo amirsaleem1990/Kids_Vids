0f4aad6c99ab1f7285e3bff8e1cced872c4acd64
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-12-08 12:13:38 +0500

    added <'noplaylist' : True>

--- get_channel_name.py
+++ get_channel_name.py
@@ -1,11 +1,11 @@
-#!/home/amir/.local/bin/ipython3
+#!/usr/bin/ipython3
 import youtube_dl
 import sys
 try:
 	url = sys.argv[1]
 except:
 	url = input("Enter url: ")
-yt = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+yt = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s', 'noplaylist' : True})
 info = yt.extract_info(url, download=False)
 
 print(
