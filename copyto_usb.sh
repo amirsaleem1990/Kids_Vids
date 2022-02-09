3ff7ccd5eb81fb4df0fa6a39c053443cc838d681
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-02-09 19:21:35 +0500

    last time me ny usb me videos move karin, phir usb ghar me laga kar dekhi to wo corrupt ho gai thi, to meri sari videos zaya ho gain, ab me files ko remove nahi karun ga, balky copy karun ga, or 'last_copied_files.txt' me copied files k name likh lun ga, agar koi problem nahi hwa to next time 'last_copied_files.txt' me mentioned files ko remove kar dun ga

--- copyto_usb.sh
+++ copyto_usb.sh
@@ -6,9 +6,22 @@ lsblk -e7
 echo 
 read -p "Enter usb mount point (eg: /home/home/usb): " usb_mounted_at
 echo
-mv $(for i in `ls` ; do \
-		if [[ $(echo $i | grep -o '\.' | wc -l) -eq 1 ]] ; then 
-			echo $i
-		fi
-	done
-	) "$usb_mounted_at" -v
+read -p "Enter dev (eg: /dev/sdc): " dev_
+echo 
+
+rm -f last_copied_files.txt
+
+IFS=$'\n'
+for file in $(
+				for i in `ls` ; do \
+					if [[ $(echo $i | grep -o '\.' | wc -l) -eq 1 ]] ; then 
+						echo $i
+					fi
+				done
+			); do 
+	if [[ $(df $dev_ | sed 's/\ \{2,\}/,/g' | cut -d, -f4 | cut -d\% -f1) -gt 95 ]] ; then
+		exit
+	fi
+	cp "$file" "$usb_mounted_at" -v
+	echo "$file" >> last_copied_files.txt
+done
