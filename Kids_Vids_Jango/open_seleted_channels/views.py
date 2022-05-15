59e23e6d5ec68882a13fe33d719e1a775663c5a4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-05-15 12:21:02 +0500

    u

--- Kids_Vids_Jango/open_seleted_channels/views.py
+++ Kids_Vids_Jango/open_seleted_channels/views.py
@@ -1,14 +1,13 @@
+import getpass
 from django.shortcuts import render
-from django.http.response import HttpResponse
-import getpass
-import json
-import shutil
-import getpass
-import pickle
-import sys
+# from django.http.response import HttpResponse
+# import json
+# import shutil
+# import pickle
+# import sys
 
 def open_seleted_channels(request):
 	x = dict(request.POST)
 	print("-----------------------------------------!!!!!!!!!!!!!!!!!!")
 	print(x)
-	# return render(request, 'choose_channels.html', {"channels" : list_of_channels})
+	return render(request, 'choose_channels.html', {"channels" : list_of_channels})
